fecea3fd1b2f1e55dc195ef93e818910b205c9f9
Gerardo Garavito <garavitogerardo@gmail.com>
2020-12-15 16:11:55 -0600

    setted up the api files and now you can see it using /api/avo/

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,7 +1,14 @@
 import { IncomingMessage, ServerResponse} from 'http';
+import DB from '@database'
 
 const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
-  response.end(JSON.stringify({ hello: 'world' }))
+  const db = new DB()
+  const allEntries = await db.getAll()
+  const length = allEntries.length
+
+  response.statusCode = 200
+  response.setHeader('Content-type', 'application/json')
+  response.end(JSON.stringify({ length, data: allEntries }))
 }
 
 export default allAvos
